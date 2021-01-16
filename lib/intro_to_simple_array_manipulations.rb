3771bb9bf414fc570e8896a38e50b0f142d83728
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-16 19:38:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ array.pop
 
 end
 
-def pop_with_args (array, string)
+def pop_with_args (array)
 dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
 small_dogs = ["Chihuahua", "Shiba Inu"]
 dog_breeds.pop(2)
